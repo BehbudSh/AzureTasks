0eaa3b434137ea14ab19880a21cef09b9926f14e
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-04-05 17:50:18 +0400

    Adding a few parameters to Task1

--- Azure1/azuretask1.ps1
+++ Azure1/azuretask1.ps1
@@ -1,32 +1,27 @@
-Connect-AzAccount
-
+param(
+    [Parameter(Mandatory = $true)]
+    [String]$ResourceGroup,
+    [Parameter(Mandatory = $true)]
+    [String]$StorageAccountName,
+    [Parameter(Mandatory = $true)]
+    [String]$ContainerName,
+    [Parameter(Mandatory = $true)]
+    [String]$LocalDir
+)
+Import-AzContext -Path D:\credintial.json
 # Create a new resource group.
-
-$resourceGroup = "azureResource"
-$location = "eastus"
-New-AzResourceGroup -Name $resourceGroup -Location $location 
-
+$Location = "WestEurope"
+New-AzResourceGroup -Name $ResourceGroup -Location $Location 
 # Set the name of the storage account and the SKU name. 
-
-$storageAccountName = "behbud"
-$skuName = "Standard_LRS"
-
+$SkuName = "Standard_LRS"
 # Create the storage account.
-
-New-AzStorageAccount -ResourceGroupName $resourceGroup -Name $storageAccountName -Location $location -SkuName $skuName
-
+New-AzStorageAccount -ResourceGroupName $ResourceGroup -Name $StorageAccountName -Location $Location -SkuName $SkuName
 #Create the blob container
-
-$StorageAccountName = "behbud" 
-$StorageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name $storageAccountName).Value[0]
-$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
-$ContainerName = "upload"
-New-AzStorageContainer -Name $ContainerName -Context $ctx -Permission Blob
-
+$StorageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $ResourceGroup -Name $StorageAccountName).Value[0]
+$Ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+New-AzStorageContainer -Name $ContainerName -Context $Ctx -Permission Blob
 #Uploading files and folders to Container
-
-$localDir = "C:\Users\Behbuds\Desktop\upload\"
-foreach($file in (Get-ChildItem $localDir -File -Recurse)){
-   $blobName = $file.FullName.Remove(0,24).Replace("\","/")
-   Set-AzStorageBlobContent -Blob $blobName -Container $ContainerName -File $($file.FullName) -Context $ctx
+foreach ($File in (Get-ChildItem $LocalDir -File -Recurse)) {
+    $BlobName = $File.FullName.Remove(0, 24).Replace("\", "/")
+    Set-AzStorageBlobContent -Blob $BlobName -Container $ContainerName -File $($File.FullName) -Context $Ctx
 }

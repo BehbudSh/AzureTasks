5cad9a7c34d451a740832b5a700d3117914f3f43
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-05-04 19:22:28 +0400

    Get-Module

--- Install-module.ps1
+++ Install-module.ps1
@@ -1,6 +1,6 @@
 $Modules = @('xPSDesiredStateConfiguration', 'xNetworking', 'StorageDsc', 'ComputerManagementDsc')
 foreach ($Module in $Modules) {
-    if (!(Find-Module $Module)) {
-        Install-Module $Module
+    if (!(Get-Module $Module)) {
+        Install-Module -Verbose $Module -Force
     }
 }

6ef7ad45d4ae91ab802c202f7488f9f5127851d9
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-05-13 12:13:35 +0400

    Adding new modules

--- Install-module.ps1
+++ Install-module.ps1
@@ -1,4 +1,4 @@
-$Modules = @('xPSDesiredStateConfiguration', 'xNetworking', 'StorageDsc', 'ComputerManagementDsc')
+$Modules = @('xPSDesiredStateConfiguration', 'xNetworking', 'StorageDsc', 'ComputerManagementDsc','xCertificate','xWebAdministration')
 foreach ($Module in $Modules) {
     if (!(Get-Module $Module)) {
         Install-Module -Verbose $Module -Force
